7a32b3b4a04d04dafe254d1e7000fe700ba6cfc7
Jeff <jm@jeffz.com>
2021-07-13 04:46:29 -0400

    Add React style for car color

--- src/car.js
+++ src/car.js
@@ -3,8 +3,9 @@ import React from "react";
 class Car extends React.Component {
   render() {
     const { color, year, make, model, tag } = this.props.car;
+    const style = { color: color };
     return (
-      <h2>
+      <h2 style={style}>
         Hi, I am a {color} {year} {make} {model} with tag {tag}
       </h2>
     );
