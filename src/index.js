4a695cfe1aef2154d7e4c404b07d3b34166cc7e0
Jeff <jm@jeffz.com>
2021-07-12 20:08:26 -0400

    Car with State and Props

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Car extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "red",
+    };
+  }
+
   render() {
-    return <h2>Hi, I am a Car!</h2>;
+    return (
+      <h2>
+        Hi, I am {this.state.color} Car made by {this.props.make}!
+      </h2>
+    );
   }
 }
 
-ReactDOM.render(<Car />, document.getElementById("root"));
+ReactDOM.render(<Car make="Ford" />, document.getElementById("root"));
