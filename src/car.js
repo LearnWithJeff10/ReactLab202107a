92fd77628702c38c50074e48bcd59b1647221f92
Jeff <jm@jeffz.com>
2021-07-12 22:27:40 -0400

    Made color user-defined

--- src/car.js
+++ src/car.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Car extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      color: "red",
-    };
-  }
-
   render() {
     return (
       <h2>
-        Hi, I am {this.state.color} car made by {this.props.make}!
+        Hi, I am {this.props.color} car made by {this.props.make}!
       </h2>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import Garage from "./garage";
 
 ReactDOM.render(
   <div>
-    <Garage owner="Jeff" car1="Ford" car2="Chevy" />
-    <Garage owner="Elon" car1="Honda" car2="Hyundai" />
+    <Garage owner="Jeff" car1="Ford" color1="red" car2="Chevy" color2="blue" />
+    <Garage
+      owner="Elon"
+      car1="Honda"
+      color1="green"
+      car2="Hyundai"
+      color2="black"
+    />
   </div>,
   document.getElementById("root")
 );
