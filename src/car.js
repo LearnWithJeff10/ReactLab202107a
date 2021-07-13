98b1755c086bf17ce8549dab3b4c25ddb5b4d916
Jeff <jm@jeffz.com>
2021-07-13 05:11:57 -0400

    Add CSS stylesheet and classes

--- src/car.js
+++ src/car.js
@@ -4,7 +4,7 @@ class Car extends React.Component {
   render() {
     const { color, year, make, model, tag } = this.props.car;
     return (
-      <h2 style={{ color: color }}>
+      <h2 className="car" style={{ color: color }}>
         Hi, I am a {color} {year} {make} {model} with tag {tag}
       </h2>
     );
--- src/garage.js
+++ src/garage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Car from "./car";
+import "./garage.css";
 
 class Garage extends React.Component {
   render() {
     const { owner, cars } = this.props.ginfo;
     return (
-      <div>
+      <div className="garage">
         <h1>{owner}&apos;s Garage</h1>
         {cars.map((car) => (
           <Car car={car} />
