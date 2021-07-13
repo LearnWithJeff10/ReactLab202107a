1ec5f67e723818068dd8764ccd7f243eeda062e5
Jeff <jm@jeffz.com>
2021-07-13 04:47:43 -0400

    Simplify style for car

--- src/car.js
+++ src/car.js
@@ -3,9 +3,8 @@ import React from "react";
 class Car extends React.Component {
   render() {
     const { color, year, make, model, tag } = this.props.car;
-    const style = { color: color };
     return (
-      <h2 style={style}>
+      <h2 style={{ color: color }}>
         Hi, I am a {color} {year} {make} {model} with tag {tag}
       </h2>
     );
