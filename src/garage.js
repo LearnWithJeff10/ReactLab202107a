48d1c9efc22030ebee660ef3c4d42ad2a20849bb
Jeff <jm@jeffz.com>
2021-07-14 15:28:02 -0400

    Use Bootstrap

--- src/garage.js
+++ src/garage.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Car from "./car";
-import "./garage.css";
+//import "./garage.css";
 
 class Garage extends React.Component {
   render() {
     const { owner, cars } = this.props.ginfo;
     return (
-      <div className="garage">
-        <h1>{owner}&apos;s Garage</h1>
-        {cars.map((car) => (
-          <Car car={car} />
-        ))}
+      <div className="card mb-3">
+        <div className="garage card-body bg-light">
+          <h1 className="card-title text-light bg-primary pl-1">
+            {owner}&apos;s Garage
+          </h1>
+          {cars.map((car) => (
+            <Car car={car} />
+          ))}
+        </div>
       </div>
     );
   }
