589cbb2b4cdcb431c041ac7854d4ec784c901a12
Jeff <jm@jeffz.com>
2021-07-14 21:39:50 -0400

    First attempt at style

--- src/car.js
+++ src/car.js
@@ -3,8 +3,9 @@ import React from "react";
 class Car extends React.Component {
   render() {
     const { make, model, year, color, tag } = this.props.car;
+
     return (
-      <h2>
+      <h2 style={{ color: "blue" }}>
         I am a {color} {year} {make} {model} with tag {tag}!
       </h2>
     );
