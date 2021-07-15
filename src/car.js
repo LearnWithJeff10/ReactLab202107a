45853961850dd71d0ca63e5c1c219e9ecbaff8f1
Jeff <jm@jeffz.com>
2021-07-15 01:15:15 -0400

    Event handing 1 - Add a "car needs work" button

--- src/car.js
+++ src/car.js
@@ -1,12 +1,28 @@
 import React from "react";
+import "./car.css";
 
 class Car extends React.Component {
+  state = {
+    needsWork: false,
+  };
+
+  doSomething = () => {
+    console.log("Setting needsWork to true");
+    this.setState({ needsWork: true });
+  };
+
   render() {
     const { make, model, year, color, tag } = this.props.car;
 
     return (
-      <h2 style={{ color: color }}>
+      <h2
+        className={this.state.needsWork ? "needswork" : " "}
+        style={{ color: color }}
+      >
         I am a {color} {year} {make} {model} with tag {tag}!
+        <button onClick={this.doSomething} className="btn btn-sm btn-warning">
+          Not ready
+        </button>
       </h2>
     );
   }
